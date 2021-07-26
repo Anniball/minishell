d864587a67abf53e2ca6f2a28fb7f25dceaf1a81
Lucas Delmas <ldelmas@fu-r9-p5.s19.be>
2021-07-26 10:45:44 +0200

    builtin 'env' done

--- srcs/rebuilt/env.c
+++ srcs/rebuilt/env.c
@@ -1,46 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldelmas <ldelmas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/26 09:45:11 by ldelmas           #+#    #+#             */
-/*   Updated: 2021/07/26 10:38:51 by ldelmas          ###   ########.fr       */
+/*   Created: 2021/07/26 10:39:18 by ldelmas           #+#    #+#             */
+/*   Updated: 2021/07/26 10:44:47 by ldelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int simple_strcmp(char *s1, char *s2)
+int	 get_env(char **env)
 {
 	int i;
-	
-	if (!s1 || !s2)
-		return (-1);
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] != 0 && s2[i] != 0)
-		i++;
-	return (s1[i] - s2[i]);
-}
 
-int get_echo(t_cmd *cmd)
-{
-	int		pos;
-	char	end;
-
-	pos = 1;
-	end = '\n';
-	if (!simple_strcmp(cmd->flags[1], "-n"))
+	i = -1;
+	while (env[++i])
 	{
-		pos++;
-		end = '\0';
+		write(STDOUT_FILENO, env[i], my_strlen(env[i]));
+		write(1, "\n", 1);
 	}
-	while (cmd->flags[pos])
-	{
-		write(STDOUT_FILENO, cmd->flags[pos], my_strlen(cmd->flags[pos]));
-		pos++;	
-	}
-	write(STDOUT_FILENO, &end, 1);
 	return (0);
 }
