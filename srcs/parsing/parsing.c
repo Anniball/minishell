10a092e6187a9975da02561ea2e01ffa0646c129
TimourP <petit.timour@gmail.com>
2021-08-02 15:59:01 +0200

    parsing almost ok

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 14:57:39 by tpetit            #+#    #+#             */
-/*   Updated: 2021/08/02 15:07:23 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/08/02 15:54:01 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,6 +151,127 @@ char *get_env_value(char **env, char *var)
 	return my_strdup("");
 }
 
+int	is_in_str(char *str, char *c_lst)
+{
+	int i;
+	int j;
+
+	i = -1;
+	while (str[++i])
+	{
+		j = -1;
+		while (c_lst[++j])
+			if (c_lst[j] == str[i])
+				return (1);	
+	}
+	return (0);
+}
+
+char	*strdup_until(const char *str, char c)
+{
+	int		i;
+	char	*dest;
+
+	i = -1;
+	while (str[++i] && str[i] != c)
+		;
+	dest = malloc((i + 1) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	i = -1;
+	while (str[++i] && str[i] != c)
+		dest[i] = str[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*parse_join(char *s1, char *s2)
+{
+	int		len;
+	int		i;
+	char	*conc_str;
+
+	len = my_strlen(s1) + my_strlen(s2);
+	if (!(conc_str = malloc(sizeof(char) * (len + 1))))
+	{
+		free(s1);
+		return (NULL);
+	}
+	conc_str[len] = 0;
+	i = -1;
+	len = my_strlen(s1);
+	if (len > 0)
+		while (++i < len)
+			conc_str[i] = s1[i];
+	i = -1;
+	if (my_strlen(s2) > 0)
+		while (s2[++i])
+			conc_str[i + len] = s2[i];
+	free(s1);
+	return (conc_str);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	int		i;
+	size_t	str_len;
+	char	*str;
+
+	if (!s)
+		return (0);
+	if (start > my_strlen(s))
+	{
+		if (!(str = malloc(sizeof(char))))
+			return (NULL);
+		str[0] = 0;
+	}
+	else
+	{
+		i = -1;
+		str_len = len < my_strlen(s) - start ? len : my_strlen(s) - start;
+		if (!(str = malloc(sizeof(char) * (str_len + 1))))
+			return (NULL);
+		while (s[++i] && i < (int)len)
+			str[i] = s[start + i];
+		str[str_len] = 0;
+	}
+	return (str);
+}
+
+char *replace_by_env_value(char **env, char *str)
+{
+	char	open_quote;
+	int		i;
+	char	*new_str;
+	char	*env_var;
+	int		last_join;
+
+	i = -1;
+	new_str = NULL;
+	last_join = 0;
+	open_quote = 0;
+	while (str[++i])
+	{
+		if (str[i] == '"' || str[i] == '\'')
+		{
+			if (open_quote == str[i])
+				open_quote = 0;
+			else if (open_quote == 0)
+				open_quote = str[i];
+		}
+		if (str[i] == '$' && str[i + 1])
+		{
+			new_str = parse_join(new_str, ft_substr(str, last_join, i - last_join));
+			env_var = strdup_until(&str[i + 1], ' ');
+			new_str = parse_join(new_str, get_env_value(env, env_var));
+			last_join = i + my_strlen(env_var) + 1;
+		}
+	}
+	if (last_join != i)
+		new_str = parse_join(new_str, ft_substr(str, last_join, i - last_join));
+	return new_str;
+}
+
 int	parse_line(t_shell *shell, char *line)
 {
 	char **split_line;
@@ -162,7 +283,7 @@ int	parse_line(t_shell *shell, char *line)
 	cmd_clear(&shell->start_cmd);
 	shell->start_cmd = NULL;
 	split_line = parse_split(line, '|');
-	printf("getenv: %s\n", get_env_value(shell->env, line));
+	printf("val: %s\n", replace_by_env_value(shell->env, line));
 	while (split_line[++i] != NULL)
 	{
 		strip = my_strip(split_line[i], ' ');
