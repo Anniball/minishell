ce0cf45a03f6e161541c2b01340729dbf7d2965c
anniball <lucas.delmas.an123@gmail.com>
2021-08-18 10:57:16 +0200

    exit builtin get arguments arn right errors

--- srcs/pipe/builtins.c
+++ srcs/pipe/builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpetit <tpetit@student.s19.be>             +#+  +:+       +#+        */
+/*   By: ldelmas <ldelmas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 08:50:44 by ldelmas           #+#    #+#             */
-/*   Updated: 2021/08/13 09:41:36 by tpetit           ###   ########.fr       */
+/*   Updated: 2021/08/18 10:54:48 by ldelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,62 @@ static int	simple_strcmp(char *s1, char *s2)
 	while (s1[i] == s2[i] && s1[i] != 0 && s2[i] != 0)
 		i++;
 	return (s1[i] - s2[i]);
+}
+
+static int	atoc(char *str, unsigned char *status)
+{
+	size_t			i;
+	char			neg;
+	unsigned long	val;
+
+	i = 0;
+	neg = 0;
+	val = 0;
+	if (str[i] == '-')
+		neg = 1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+		val *= 10;
+		val += (str[i] - '0');
+		if ((val > 2147483647 && !neg) || (val > 2147483648 && neg))
+			return (-1);
+		i++;
+	}
+	if (neg)
+		val *= -1;
+	*status = (unsigned char)val;
+	return (0);
+}
+
+static int	exec_exit(t_cmd *cmd)
+{
+	unsigned char	exit_status;
+
+	if (cmd->flags[1])
+	{
+		if (atoc(cmd->flags[1], &exit_status) < 0)
+		{
+			write(STDERR_FILENO, "minishell: exit: ", 17);
+			write(STDERR_FILENO, cmd->flags[1], my_strlen(cmd->flags[1]));
+			write(STDERR_FILENO, ": numeric argument required\n", 28);
+			get_exit(2, cmd);
+		}
+		else if (cmd->flags[2])
+		{
+			write(STDERR_FILENO, "exit\n", 6);
+			write(STDERR_FILENO, "minishell: exit: too many arguments\n", 36);
+			return (MY_FILE_NOT_FOUND);
+		}
+		else
+			get_exit(exit_status, cmd);
+	}
+	else
+		get_exit(EXIT_SUCCESS, cmd);
+	return (-1);
 }
 
 int	exec_builtin(t_cmd *cmd, char ***env)
@@ -42,12 +98,7 @@ int	exec_builtin(t_cmd *cmd, char ***env)
 	else if (!simple_strcmp(cmd->cmd, "unset"))
 		ret = get_unset(cmd, env);
 	else if (!simple_strcmp(cmd->cmd, "exit"))
-	{
-		if (!cmd->next)
-			ret = get_exit(EXIT_SUCCESS, cmd);
-		else
-			ret = get_exit(EXIT_SUCCESS, cmd);
-	}
+		ret = exec_exit(cmd);
 	if (ret != 257 && init_edit_shell(2, NULL, 0)->start_cmd->next)
 		get_exit(EXIT_SUCCESS, cmd);
 	return (ret);
